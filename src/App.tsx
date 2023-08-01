f7ab8f41dc04ad1c1300633c08c596988fbb8b35
Sean Bird <bird.seanmichael@gmail.com>
2023-07-31 22:35:05 -0600

    Remove scrollbar in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ const COLORS = [
   'cyan',
   'pink',
   'teal',
-  'zinc',
   'rose',
 ]
 const COLOR = COLORS[Math.floor(Math.random() * COLORS.length)]
@@ -43,9 +42,9 @@ function App() {
   const sendMessage = async (text: string) => {
     const updatedChat: Message[] = [...messages, {content: text, role: 'user'}]
     setMessages(updatedChat)
+    messageBoxRef.current!.value = ''
     const response = await Chat.getCompletion(updatedChat.map(msg => {return {content: msg.content, role: msg.role}}))
     setMessages([...updatedChat, {content: response, role: 'assistant'}])
-    messageBoxRef.current!.value = ''
 
     setTimeout(() => {
       scrollBoxRef.current!.scrollTop = scrollBoxRef.current!.scrollHeight
@@ -69,28 +68,30 @@ function App() {
   return (
     <div className="fixed w-full h-full p-5 bg-gradient-to-br from-black to-slate-800">
       <div className='fixed top-0 w-full h-1/6 bg-gradient-to-b from-black to-transparent z-10'/>
-      <div className='relative flex flex-col h-5/6 leading-tight'>
-        <div className='absolute bottom-0 h-full overflow-y-auto overflow-x-hidden w-full' ref={scrollBoxRef}>
+      <div className='relative flex flex-col h-5/6 leading-tight items-center'>
+        <div className='absolute bottom-0 h-full overflow-y-auto overflow-x-hidden w-full max-w-prose' ref={scrollBoxRef}>
           <p className='py-12'/>
           {renderMessages()}
         </div>
       </div>
-      <div className='relative h-1/6 flex flex-col'>
-        <div className='flex flex-row leading-none mb-5 text-sm'>
-          <QuestionPill question={`What is ${PRODUCT_NAME}?`} color={COLOR} sendMessage={sendMessage}/>
-          <QuestionPill question='How does it work?' color={COLOR} sendMessage={sendMessage}/>
-          <QuestionPill question='Who are you?' color={COLOR} sendMessage={sendMessage}/>
-        </div>
-        <div className='w-full h-1/2 outline outline-white flex flex-row'>
-          <textarea 
-            className={`bg-black caret-${COLOR}-400 text-${COLOR}-400 p-3 w-5/6 m-0`} 
-            placeholder='Enter a message to start...'
-            ref={messageBoxRef}
-          />
-          <button 
-            className={`bg-${COLOR}-400 p-3 w-1/6`}
-            onClick={() => sendMessage(messageBoxRef.current!.value)}
-          >Send</button>
+      <div className='flex items-center relative h-1/6 flex-col'>
+        <div className='max-w-prose'>
+          <div className='flex flex-row leading-none mb-5 text-sm'>
+            <QuestionPill question={`What is ${PRODUCT_NAME}?`} color={COLOR} sendMessage={sendMessage}/>
+            <QuestionPill question='How does it work?' color={COLOR} sendMessage={sendMessage}/>
+            <QuestionPill question='Who are you?' color={COLOR} sendMessage={sendMessage}/>
+          </div>
+          <div className='w-full h-1/2 outline outline-white flex flex-row'>
+            <textarea 
+              className={`bg-black caret-${COLOR}-400 text-${COLOR}-400 p-3 w-5/6 m-0`} 
+              placeholder='Enter a message to start...'
+              ref={messageBoxRef}
+            />
+            <button 
+              className={`bg-${COLOR}-400 p-3 w-1/6`}
+              onClick={() => sendMessage(messageBoxRef.current!.value)}
+            >Send</button>
+          </div>
         </div>
       </div>
       <span className='text-red-400 bg-red-400'/>
@@ -105,6 +106,7 @@ function App() {
       <span className='text-teal-400 bg-teal-400'/>
       <span className='text-zinc-400 bg-zinc-400'/>
       <span className='text-rose-400 bg-rose-400'/>
+      <span className='text-5xl'/>
     </div>
   );
 }
