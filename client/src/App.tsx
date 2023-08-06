72dd72f40d7fd376f0c5689f1c47fa2d036fd018
Sean Bird <bird.seanmichael@gmail.com>
2023-08-05 18:29:13 -0600

    Refactor question pill button styling to be more responsive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,10 +30,11 @@ function App() {
 
   const messageBoxRef = React.useRef<HTMLTextAreaElement>(null)
   const scrollBoxRef = React.useRef<HTMLDivElement>(null)
+  const formRef = React.useRef<HTMLFormElement>(null)
 
   const renderMessages = () => {
     return messages.map((message, index) => {
-      const emphasizedMsg = {__html: message.content.replace(/<(.+?)>/g, `<span class=\'text-${message.role === 'user' ? '' : COLOR}-400\'>$1</span>`)}
+      const emphasizedMsg = {__html: message.content.replace(/<(.+?)>/g, `<span class='text-${message.role === 'user' ? '' : COLOR}-400'>$1</span>`)}
       return (
         <p key={index} className={`mb-6 break-words ${message.role === 'user' ? `text-${COLOR}-400` : 'text-white'} text-${message.fontSize || 'xl'}`} dangerouslySetInnerHTML={emphasizedMsg}/>
       )
@@ -69,33 +70,48 @@ function App() {
     }
   })
 
+  // on form submit, send message
+  React.useEffect(() => {
+    const handleSubmit = (event: Event) => {
+      event.preventDefault()
+      sendMessage(messageBoxRef.current!.value)
+    }
+    formRef.current!.addEventListener('submit', handleSubmit)
+    return () => {
+      formRef.current!.removeEventListener('submit', handleSubmit)
+    }
+  })
+
   return (
     <div className="fixed w-full h-full p-5 bg-gradient-to-br from-black to-slate-800">
       <div className='fixed top-0 w-full h-1/6 bg-gradient-to-b from-black to-transparent z-10'/>
       <div className='relative flex flex-col h-5/6 leading-tight items-center'>
-        <div className='absolute bottom-0 h-full overflow-y-auto overflow-x-hidden w-full max-w-prose' ref={scrollBoxRef}>
+        <div className='absolute bottom-0 h-full overflow-y-auto overflow-x-hidden w-full' ref={scrollBoxRef}>
           <p className='py-12'/>
           {renderMessages()}
         </div>
       </div>
       <div className='flex items-center relative h-1/6 flex-col'>
-        <div className='max-w-prose'>
+        <div className=''>
           <div className='flex flex-row leading-none mb-5 text-sm'>
             <QuestionPill question={`What is ${PRODUCT_NAME}?`} color={COLOR} sendMessage={sendMessage}/>
             <QuestionPill question='How does it work?' color={COLOR} sendMessage={sendMessage}/>
             <QuestionPill question='Who are you?' color={COLOR} sendMessage={sendMessage}/>
           </div>
-          <div className='w-full h-1/2 outline outline-white flex flex-row'>
+          <form 
+            className='w-full h-1/2 outline outline-1 outline-white rounded flex flex-row'
+            ref={formRef}
+          > 
             <textarea 
-              className={`bg-black caret-${COLOR}-400 text-${COLOR}-400 p-3 w-5/6 m-0`} 
+              className={`bg-black caret-${COLOR}-400 text-${COLOR}-400 p-3 w-5/6 m-0 resize-none`} 
               placeholder='Enter a message to start...'
               ref={messageBoxRef}
             />
             <button 
               className={`bg-${COLOR}-400 p-3 w-1/6`}
-              onClick={() => sendMessage(messageBoxRef.current!.value)}
+              formAction='submit'
             >Send</button>
-          </div>
+          </form>
         </div>
       </div>
       <span className='text-red-400 bg-red-400'/>
--- client/src/QuestionPill/QuestionPill.tsx
+++ client/src/QuestionPill/QuestionPill.tsx
@@ -9,7 +9,7 @@ interface QuestionPillProps {
 function QuestionPill(props: QuestionPillProps) {
     return (
         <button 
-            className={`rounded-full bg-${props.color}-400 p-1 m-2 px-2 outline-white`}
+            className={`rounded-full bg-${props.color}-400 p-1 m-2 px-2 md:p-2 outline-white`}
             onClick={() => props.sendMessage(props.question)}
         >{props.question}</button>
     )
