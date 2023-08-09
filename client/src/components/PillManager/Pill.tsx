469556e68ccd86e4b7c54bf1b5d994e8911da93a
Sean Bird <bird.seanmichael@gmail.com>
2023-08-08 22:40:53 -0600

    Add new default pills and hide extra pills on mobile view

--- client/src/components/PillManager/Pill.tsx
+++ client/src/components/PillManager/Pill.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 interface PillProps {
     text: string
     color: string
+    className?: string
     onClick: (message: string) => void
 }
 
 function Pill(props: PillProps) {
     return (
         <button 
-            className={`rounded-full bg-${props.color}-400 p-1 m-2 px-2 md:p-2 outline-white`}
+            className={`${props.className || ''} rounded-full bg-${props.color}-400 p-1 m-2 px-2 md:p-2 outline-white h-[36px]`}
             onClick={() => props.onClick(props.text)}
         >{props.text}</button>
     )
--- client/src/components/PillManager/PillManager.tsx
+++ client/src/components/PillManager/PillManager.tsx
@@ -13,10 +13,37 @@ interface PillManagerProps {
   sendMessage: (message: string) => void;
 }
 
+/**'1. What makes [Product Name] special?\n' +
+    '2. Why choose [Product Name]?\n' +
+    '3. How is [Product Name] different?\n' +
+    '4. Can [Product Name] help me?\n' +
+    '5. Show me a [Product Name] demo?\n' +
+    '6. Is [Product Name] customizable?\n' +
+    '7. How secure is [Product Name]?\n' +
+    '8. Does [Product Name] save time?\n' +
+    '9. Who uses [Product Name]?\n' +
+    "10. What's the cost of [Product Name]?\n" +
+    '11. Any discounts or offers?\n' +
+    '12. Can I trust [Product Name]?\n' +
+    '13. Is [Product Name] user-friendly?\n' +
+    '14. Where is [Product Name] used?\n' +
+    '15. Does [Product Name] have support?\n' +
+    '16. Are updates included?\n' +
+    '17. What if I need help?\n' +
+    '18. Can anyone use [Product Name]?\n' +
+    '19. Are there [Product Name] tutorials?\n' +
+    '20. What have others said?' */
+
 const defaultPills: PillConfig[] = [
     { text: `What is ${PRODUCT_NAME}?` },
     { text: "How does it work?" },
     { text: "Who are you?" },
+    { text: `How is it different?`},
+    { text: `Show me a demo?`},
+    { text: `Is it customizable?`},
+    { text: `What does it cost?`},
+    { text: `Any discounts?`},
+    { text: `Can I trust ${PRODUCT_NAME}?`},
 ]
 
 function PillManager(props: PillManagerProps) {
@@ -30,12 +57,16 @@ function PillManager(props: PillManagerProps) {
   }
 
   const renderedPills = pills?.map((pill, index) => {
+    if (index > 4) {
+        return <></>
+    }
     return (
       <Pill
         key={index}
         text={pill.text}
         color={props.color}
         onClick={onClick}
+        className={index > 2 ? 'hidden md:inline' : ''}
       />
     );
   });
