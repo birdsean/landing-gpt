7ee8d743e68382d76fce5bea57e61547ae4ad6ca
Sean Bird <bird.seanmichael@gmail.com>
2023-08-12 05:31:33 -0600

    init react router & abstraction of layout component

--- client/src/components/PillManager/PillManager.tsx
+++ client/src/components/PillManager/PillManager.tsx
@@ -4,7 +4,7 @@ import { PRODUCT_NAME } from "../../helpers/variables";
 
 interface PillConfig {
   text: string;
-  hyperlink?: string;
+  route?: string;
   hightlight?: boolean;
 }
 
@@ -14,19 +14,19 @@ interface PillManagerProps {
 }
 
 const defaultPills: PillConfig[] = [
-    { text: `What is ${PRODUCT_NAME}?` },
-    { text: "How does it work?" },
-    { text: "Who are you?" },
-    { text: `How is it different?`},
-    { text: `What does it cost?`},
-    { text: `Show me a demo?`},
-    { text: `Is it customizable?`},
-    // { text: `Any discounts?`},
-    { text: `Can I trust ${PRODUCT_NAME}?`},
-]
+  { text: `What is ${PRODUCT_NAME}?` },
+  { text: "How does it work?" },
+  { text: "Join waitlist", hightlight: true, route: "/waitlist" },
+  { text: "Who are you?" },
+  { text: `How is it different?` },
+  { text: `What does it cost?` },
+  // { text: `Show me a demo?`},
+  { text: `Is it customizable?` },
+  // { text: `Any discounts?`},
+  { text: `Can I trust ${PRODUCT_NAME}?` },
+];
 
-
-const COUNT_PILLS = 3
+const COUNT_PILLS = 3;
 
 function PillManager(props: PillManagerProps) {
   const [pills, setPills] = React.useState<PillConfig[]>(defaultPills);
@@ -36,19 +36,20 @@ function PillManager(props: PillManagerProps) {
     props.sendMessage(text);
     const newPills = pills.filter((pill) => pill.text !== text);
     setPills(newPills);
-  }
+  };
 
   const renderedPills = pills?.map((pill, index) => {
     if (index >= COUNT_PILLS) {
-        return <></>
+      return <></>;
     }
     return (
       <Pill
         key={index}
         text={pill.text}
-        color={props.color}
+        color={pill.hightlight ? 'white' : `${props.color}-400`}
         onClick={onClick}
-        className={index > COUNT_PILLS - 1 ? 'hidden md:inline' : ''}
+        path={pill.route}
+        className={index > COUNT_PILLS - 1 ? "hidden md:inline" : ""}
       />
     );
   });
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Layout from "./components/Layout/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout chatBody={<div/>} footer={<div/>} />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
