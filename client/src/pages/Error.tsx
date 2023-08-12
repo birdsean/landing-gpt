68c044df1d0d121ddca0c9835e843900d75530e7
Sean Bird <bird.seanmichael@gmail.com>
2023-08-12 06:53:39 -0600

    Refactor ErrorPage component to display error message

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
+import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const routeError = useRouteError() as { statusText?: string, message: string };
+
     const errorMessage = (
-        <div className='text-center'>
-            <h1 className="text-5xl text-white mb-4">Oops!</h1>
-            <h2 className="text-white">This page does not exist.</h2>
+        <div className='text-center text-white'>
+            <h1 className="text-5xl mb-4">Oops!</h1>
+            <p className='mb-4'>This page does not exist.</p>
+            <p>{routeError.statusText || routeError.message}</p>
         </div>
     )
 
