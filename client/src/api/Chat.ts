5e890cad0555bfc2a9a88e518c5148b173344f70
Sean Bird <bird.seanmichael@gmail.com>
2023-08-15 21:36:25 -0600

    Add DynamoDB integration to Chat API

--- client/src/api/Chat.ts
+++ client/src/api/Chat.ts
@@ -6,12 +6,22 @@ export interface ChatMessage {
 const API_URL =
   "https://h47icb3agajk2golbckv3ynmxy0ttkdg.lambda-url.us-east-1.on.aws/";
 
+const getSessionId = () => {
+  // if session id is not there, create one
+  if (!sessionStorage.getItem("sessionId")) {
+    const uuid = Math.random().toString(36).substring(2, 15);
+    sessionStorage.setItem("sessionId", uuid);
+  }
+  return sessionStorage.getItem("sessionId") || 'none';
+};
+
 export default class Chat {
   static async getCompletion(messages: ChatMessage[], appendCompletion: (completion: string) => void) {
     const response = await fetch(API_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "X-Session-Id": getSessionId(),
       },
       body: JSON.stringify(messages),
     });
--- infra/server.tf
+++ infra/server.tf
@@ -71,6 +71,33 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = aws_iam_policy.lambda_logging.arn
 }
 
+data "aws_iam_policy_document" "dynamo_messages_write" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:DeleteItem",
+    ]
+
+    resources = [aws_dynamodb_table.messages_table.arn]
+  }
+
+  depends_on = [aws_dynamodb_table.messages_table]
+}
+
+resource "aws_iam_policy" "dynamo_messages_write" {
+  name        = "dynamo_messages_write"
+  description = "IAM policy for writing to messages table"
+  policy      = data.aws_iam_policy_document.dynamo_messages_write.json
+}
+
+resource "aws_iam_role_policy_attachment" "dynamo_messages_write" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.dynamo_messages_write.arn
+}
+
 # Lambda
 
 resource "aws_lambda_function" "api_landing_gpt_function" {
@@ -94,7 +121,7 @@ resource "aws_lambda_function" "api_landing_gpt_function" {
 resource "aws_lambda_function_url" "api_url" {
   function_name      = aws_lambda_function.api_landing_gpt_function.function_name
   authorization_type = "NONE"
-  invoke_mode = "RESPONSE_STREAM"
+  invoke_mode        = "RESPONSE_STREAM"
 
   cors {
     allow_origins  = ["*"]
